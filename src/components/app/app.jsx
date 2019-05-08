3d6bdc038a9f2fce59f7197b5a3134f89dad25b1
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2019-05-09 00:34:23 +0200

    Add welcome screen

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import WelcomeScreen from '../../components/welcome-screen/welcome-screen.jsx';
 
 const App = () => {
-    return <WelcomeScreen />;
+  return <WelcomeScreen />;
 };
 
 export default App;
