055caf4a27a853b1b035ada96aea49f68167aba5
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2019-05-14 01:32:24 +0200

    Add welcome screen props

--- src/components/welcome-screen/welcome-screen.jsx
+++ src/components/welcome-screen/welcome-screen.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
-const WelcomeScreen = () => {
+const WelcomeScreen = (props) => {
+  const {time, errorCount} = props;
+
   return <section className="welcome">
     <div className="welcome__logo">
       <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83" />
@@ -11,8 +13,8 @@ const WelcomeScreen = () => {
     <h2 className="welcome__rules-title">Правила игры</h2>
     <p className="welcome__text">Правила просты:</p>
     <ul className="welcome__rules-list">
-      <li>За 5 минут нужно ответить на все вопросы.</li>
-      <li>Можно допустить 3 ошибки.</li>
+      <li>За {time} минут нужно ответить на все вопросы.</li>
+      <li>Можно допустить {errorCount} ошибки.</li>
     </ul>
     <p className="welcome__text">Удачи!</p>
   </section>;
