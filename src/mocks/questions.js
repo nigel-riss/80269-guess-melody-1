cd77bb6009044d16b53992759d5aaf606b54e208
nigel-riss <mr.kurenkov@gmail.com>
2019-05-28 17:14:51 +0200

    Add audio routes to mocks

--- src/mocks/questions.js
+++ src/mocks/questions.js
@@ -4,19 +4,19 @@ export default [
     genre: `rock`,
     answers: [
       {
-        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        src: `/music/bad-kind-of-mongrel.mp3`,
         genre: `rock`,
       },
       {
-        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        src: `/music/bluesy-mood.mp3`,
         genre: `blues`,
       },
       {
-        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        src: `/music/dawn.mp3`,
         genre: `jazz`,
       },
       {
-        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        src: `/music/differential-pressure.mp3`,
         genre: `rock`,
       },
     ],
@@ -24,7 +24,7 @@ export default [
     type: `artist`,
     song: {
       artist: `Jim Beam`,
-      src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+      src: `/music/welcome-to-the-jungle.mp3`,
     },
     answers: [
       {
